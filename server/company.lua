2bad6870a425a546229787f8aee86e4685429d00
Jim Jam <j.w.steelstructures@gmail.com>
2026-04-29 23:26:51 -0400

    jim

--- server/company.lua
+++ server/company.lua
@@ -7,23 +7,27 @@ Company = {
 function Company.Load()
     print("[Lumberjack] Loading company data...")
 
-    exports.oxmysql:execute("SELECT * FROM companies LIMIT 1", {}, function(result)
-        if result and result[1] then
-            Company.id = result[1].id
-            Company.name = result[1].name
-            Company.funds = result[1].funds
-            print("[Lumberjack] Company loaded:", Company.name, "Funds:", Company.funds)
-        else
-            exports.oxmysql:insert(
-                "INSERT INTO companies (name, funds) VALUES (?, ?)",
-                { Company.name, 0 },
-                function(insertId)
-                    Company.id = insertId
-                    print("[Lumberjack] Created new company:", Company.name)
-                end
-            )
+    exports.oxmysql:execute(
+        "SELECT * FROM companies LIMIT 1",
+        {},
+        function(result)
+            if result and result[1] then
+                Company.id = result[1].id
+                Company.name = result[1].name
+                Company.funds = result[1].funds
+                print("[Lumberjack] Company loaded:", Company.name, "Funds:", Company.funds)
+            else
+                exports.oxmysql:insert(
+                    "INSERT INTO companies (name, funds) VALUES (?, ?)",
+                    { Company.name, 0 },
+                    function(insertId)
+                        Company.id = insertId
+                        print("[Lumberjack] Created new company:", Company.name)
+                    end
+                )
+            end
         end
-    end)
+    )
 end
 
 function Company.AddFunds(amount)
